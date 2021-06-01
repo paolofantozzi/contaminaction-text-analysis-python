1a46fb142931fd6d3ad6a2e30efd32e21078e432
Paolo Fantozzi <paolo.fantozzi@gmail.com>
2021-06-01 12:21:45 +0200

    Spacy sentence span

--- spacy_parse.py
+++ spacy_parse.py
@@ -5,7 +5,7 @@ import spacy
 # Inizializzazione
 nlp = spacy.load('en_core_web_sm')
 
-sentence = "The striped bats, are hanging on their- feet for best"
+sentence = "The striped bats, are hanging on their- feet for best. Hi, how are you?"
 
 # Analisi della frase
 doc = nlp(sentence)
@@ -14,3 +14,7 @@ doc = nlp(sentence)
 lemmas = [token.lemma_ for token in doc if not token.is_punct]
 lemmas = [re.sub(r'[^A-Za-z0-9]', '', lemma) for lemma in lemmas]
 print(" ".join(lemmas))
+
+doc = nlp(sentence)
+for token in doc:
+    print(token.lemma_, token.text, token.sent.start_char, token.sent.end_char)
