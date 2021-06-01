2fb14ee30320eac945f8ed36ecb8435f1c7d5658
Paolo Fantozzi <paolo.fantozzi@gmail.com>
2021-06-01 10:50:14 +0200

    Spacy

--- spacy_parse.py
+++ spacy_parse.py
@@ -1,12 +1,16 @@
+import re
+
 import spacy
 
 # Inizializzazione
 nlp = spacy.load('en_core_web_sm')
 
-sentence = "The striped bats are hanging on their feet for best"
+sentence = "The striped bats, are hanging on their- feet for best"
 
 # Analisi della frase
 doc = nlp(sentence)
 
 # Estrazione del lemma per ogni parola e unificazione
-print(" ".join([token.lemma_ for token in doc]))
+lemmas = [token.lemma_ for token in doc if not token.is_punct]
+lemmas = [re.sub(r'[^A-Za-z0-9]', '', lemma) for lemma in lemmas]
+print(" ".join(lemmas))
