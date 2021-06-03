3f3aa7f5a56b5bd29a71190187258ea7cc5bfa85
Paolo Fantozzi <paolo.fantozzi@gmail.com>
2021-06-03 11:49:56 +0200

    Beautiful soup demo

---
+++ bs-demo.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+
+base_dir = Path('test_folder')
+for path in base_dir.glob('**/*.html'):
+    print(path.parent.name, path.name, path.stem)
+
+    with open(path, 'r') as html_file:
+        html = html_file.read()
+        soup = BeautifulSoup(html, 'html.parser')
+
+        print(soup.title.string)
+        for link in soup.find_all('a'):
+            print(link.string, link.get('href'))
+
+        for par in soup.find_all('h1'):
+            print(par.string)
+
+        content = soup.get_text()
+        print(content)
