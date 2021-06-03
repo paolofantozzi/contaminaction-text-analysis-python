1284b2998752932d1190f61c3e6990a189104b8e
Paolo Fantozzi <paolo.fantozzi@gmail.com>
2021-06-03 11:51:53 +0200

    Beautiful soup demo

--- bs-demo.py
+++ bs-demo.py
@@ -1,3 +1,4 @@
+import codecs
 from pathlib import Path
 
 from bs4 import BeautifulSoup
@@ -6,7 +7,7 @@ base_dir = Path('test_folder')
 for path in base_dir.glob('**/*.html'):
     print(path.parent.name, path.name, path.stem)
 
-    with open(path, 'r') as html_file:
+    with codecs.open(path, 'r', encoding='utf-8', errors='ignore') as html_file:
         html = html_file.read()
         soup = BeautifulSoup(html, 'html.parser')
 
